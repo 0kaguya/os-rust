965c34cfa8e4cb64c9099023f4e70d344d9e547e
0kaguya <carbonc014@gmail.com>
2019-04-27 11:20:35 +0800

    qemu debug shutdown

--- src/main.rs
+++ src/main.rs
@@ -25,3 +25,12 @@ pub extern "C" fn _start() -> ! {
     serial_println!("From Guest");
     loop {}
 }
+
+/// shutdown by write `0` to x86 io port `0xf4`.
+/// it works because of qemu device option `isa-debug-exit`.
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
+}
